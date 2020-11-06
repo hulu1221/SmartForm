6ebec789722ce61169eb37b835c85697ea1d3d2d
Hideki <61128858+hulu1221@users.noreply.github.com>
2020-11-06 14:03:07 +0700

    update report

--- src/main/java/com/bank/lienviet/domain/contract/ContractReportBuilder.java
+++ src/main/java/com/bank/lienviet/domain/contract/ContractReportBuilder.java
@@ -1,9 +1,15 @@
 package com.bank.lienviet.domain.contract;
 
+import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ContractReportBuilder {
     private Contract contract;
@@ -16,5 +22,19 @@ public class ContractReportBuilder {
         List<Contract> contractList = new ArrayList<>();
         contractList.add(this.contract);
         return new JRBeanCollectionDataSource(contractList);
+    }
+    public String exportReport() throws FileNotFoundException, JRException {
+        File file = ResourceUtils.getFile("classpath:HopDongPro.jrxml");
+        String path = "D:\\DEV\\printed reports\\";
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("formId", "CN/2019-01/MBGD");
+        parameters.put("formName", "ĐỀ NGHỊ KIÊM HỢP ĐỒNG\n" +"MỞ TÀI KHOẢN VÀ SỬ DỤNG DỊCH VỤ TÀI KHOẢN");
+        parameters.put("formInfor", "(DÀNH CHO KHÁCH HÀNG CÁ NHÂN)");
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, getReportDataSource());
+        JasperExportManager.exportReportToPdfFile(jasperPrint, path +  "//DeNghiKiemHopDong.pdf");
+        return "printed document path: " + path;
     }
 }
--- src/main/java/com/bank/lienviet/services/ContractController.java
+++ src/main/java/com/bank/lienviet/services/ContractController.java
@@ -3,15 +3,11 @@ package com.bank.lienviet.services;
 import com.bank.lienviet.domain.contract.ContractConstructor;
 import com.bank.lienviet.domain.contract.ContractReportBuilder;
 import net.sf.jasperreports.engine.*;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -20,21 +16,7 @@ public class ContractController {
     @GetMapping("/contract")
     public String generateContractReportPdf() throws FileNotFoundException, JRException {
         ContractReportBuilder report = new ContractReportBuilder(new ContractConstructor().getContract());
-        return exportReport(report);
+        return report.exportReport();
     }
 
-    public String exportReport(ContractReportBuilder report) throws FileNotFoundException, JRException {
-        File file = ResourceUtils.getFile("classpath:HopDongPro.jrxml");
-        String path = "D:\\DEV\\printed reports\\";
-        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
-
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("formId", "CN/2019-01/MBGD");
-        parameters.put("formName", "ĐỀ NGHỊ KIÊM HỢP ĐỒNG\n" +"MỞ TÀI KHOẢN VÀ SỬ DỤNG DỊCH VỤ TÀI KHOẢN");
-        parameters.put("formInfor", "(DÀNH CHO KHÁCH HÀNG CÁ NHÂN)");
-
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, report.getReportDataSource());
-        JasperExportManager.exportReportToPdfFile(jasperPrint, path +  "//DeNghiKiemHopDong.pdf");
-        return "printed document path: " + path;
-    }
 }
