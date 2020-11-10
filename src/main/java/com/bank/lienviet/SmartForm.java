eb2b58fd3551c61ea4b45a58a0d997de03167294
Hideki <61128858+hulu1221@users.noreply.github.com>
2020-11-10 17:23:57 +0700

    rename main class

--- src/main/java/com/bank/lienviet/SmartForm.java
+++ src/main/java/com/bank/lienviet/SmartForm.java
@@ -3,14 +3,12 @@ package com.bank.lienviet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.Timestamp;
-
 @SpringBootApplication
-public class LienvietApplication {
+public class SmartForm {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(LienvietApplication.class, args);
+        SpringApplication.run(SmartForm.class, args);
         System.out.println(System.currentTimeMillis());
     }
 
